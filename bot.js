6c35d1ea5b00220379a7bce1b190cd5103040aca
vampory <44905458+vampory@users.noreply.github.com>
2018-11-09 19:43:46 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,28 +1,9 @@
-const Discord = require('discord.js');
-
-const Util = require('discord.js');
-
-const getYoutubeID = require('get-youtube-id');
-
-const fetchVideoInfo = require('youtube-info');
-
-const YouTube = require('simple-youtube-api');
-
-const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
-
-const queue = new Map();
-
-const ytdl = require('ytdl-core');
-
-const fs = require('fs');
-
-const gif = require("gif-search");
-
-const bot = new Discord.Client({disableEveryone: true});
-
-const prefix = "!";
+const Discord = require("discord.js");
+const bot = new Discord.Client();
+const embed = new Discord.RichEmbed()
 /////////////////////////
 ////////////////////////
+            var prefix = "!";
 bot.on('message', function(message) {
     const myID = "427082011111325707";
     let args = message.content.split(" ").slice(1).join(" ");
