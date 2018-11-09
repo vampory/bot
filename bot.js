8d904e2f0b150bd7e66c8fd2e1577d289ac4efbf
vampory <44905458+vampory@users.noreply.github.com>
2018-11-09 19:50:48 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,55 +1,41 @@
-const Discord = require("discord.js");
-const bot = new Discord.Client();
-const embed = new Discord.RichEmbed()
-
-            var prefix = "!";
-bot.on('message', function(message) {
-    const myID = "427082011111325707";
-    let args = message.content.split(" ").slice(1).join(" ");
-    if(message.content.startsWith(prefix + "setname")) {
-                if(message.author.id !== '427082011111325707') return;
-            if(!args) return message.reply('اكتب الحالة اللي تريدها.');
-        bot.user.setUsername(args);
-        message.channel.send(':white_check_mark: Done!').then(msg => {
-    
-        });
-} else if(message.content.startsWith(prefix + "stream")) {
-                if(message.author.id !== '427082011111325707') return;
-            if(!args) return message.reply('اكتب الحالة اللي تريدها.');
-        bot.user.setGame(args , 'https://twitch.tv/6xlez1');
-        message.channel.send(':white_check_mark: Done!').then(msg => {
-        
-        });
-    } else if(message.content.startsWith(prefix + "ply")) {
-                        if(message.author.id !== '427082011111325707') return;
-            if(!args) return message.reply('اكتب الحالة اللي تريدها.');
-        bot.user.setGame(args);
-        message.channel.send(':white_check_mark: Done!').then(msg => {
-   
-        });
-    } else if(message.content.startsWith(prefix + "listen")) {
-                        if(message.author.id !== '427082011111325707') return;
-            if(!args) return message.reply('اكتب الحالة اللي تريدها.');
-        bot.user.setActivity(args, {type:'LISTENING'});
-        message.channel.send(':white_check_mark: Done!').then(msg => {
-        
-        });
-    } else if(message.content.startsWith(prefix + "watch")) {
-                        if(message.author.id !== '427082011111325707') return;
-            if(!args) return message.reply('اكتب الحالة اللي تريدها.');
-        bot.user.setActivity(args, {type:'WATCHING'});
-        message.channel.send(':white_check_mark: Done!').then(msg => {
-       
-        });
-    } else if(message.content.startsWith(prefix + "setavatar")) {
-                        if(message.author.id !== '427082011111325707') return;
-        bot.user.setAvatar(args);
-        message.channel.send(':white_check_mark: Done!').then(msg => {
-                if(!args) return message.reply('اكتب الحالة اللي تريدها.');
-       
-        });
-    }
+const Discord = require('discord.js'),
+    bot = new Discord.Client({sisableEveryone: true})
+    console.log("Taino");
+bot.on('guildMemberAdd', member => {
+const mohamed= member.guild.channels.get("488013994410377226");
+if(!mohamed) return;
+if(mohamed) {
+setTimeout(() => mohamed.send(`**Welcome to Pax ..**`), 4000)        
+}
 });
+ 
+const client = new Discord.Client();
 
+
+
+
+ const devs = ['475070652727033858' , '427082011111325707' , ''];
+const adminprefix = "!";
+client.on('message', message => {
+    var argresult = message.content.split(` `).slice(1).join(' ');
+      if (!devs.includes(message.author.id)) return;
+      
+  if (message.content.startsWith(adminprefix + 'ply')) {
+    client.user.setGame(argresult);
+      message.channel.sendMessage(`**  ${argresult} تم تغيير الحاله الي :white_check_mark:**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'wt')) {
+  client.user.setActivity(argresult, {type:'WATCHING'});
+      message.channel.sendMessage(`**  ${argresult} تم تغيير الحاله الي :white_check_mark:**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'ls')) {
+  client.user.setActivity(argresult , {type:'LISTENING'});
+      message.channel.sendMessage(`**  ${argresult} تم تغيير الحاله الي :white_check_mark:**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'st')) {
+    client.user.setGame(argresult, "https://www.twitch.tv/ImD3s_x");
+      message.channel.sendMessage(`**  ${argresult} تم تغيير الحاله الي :white_check_mark:**`)
+  }
+  });
 bot.login(process.env.BOT_TOKEN);
 
