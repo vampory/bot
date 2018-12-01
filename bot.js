dd23dcdbdffa2a9a8e2389fa1cf70b9ec7d1e96e
vampory <44905458+vampory@users.noreply.github.com>
2018-12-02 01:47:30 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,17 +1,1 @@
 
-
-const Discord = require('discord.js');
-const client = new Discord.Client();
-
-client.on('message', async msg=> {
-  if (msg.content === 'امر السبام') {
-
-setInterval(function() {
-msg.channel.send(` 0823t7cawfawfa 1`);
-});
-}
-});
- 
-  
-client.login(process.env.BOT_TOKEN);
-
