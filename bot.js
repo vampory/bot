e1ada35e6b711cf14a2542682215e1014f1f12f5
vampory <44905458+vampory@users.noreply.github.com>
2018-12-06 22:40:20 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,36 +1,71 @@
+
 const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("IM READY.!");
 
-const Util = require('discord.js');
-    client = new Discord.Client({sisableEveryone: true})
- const devs = ['','475070652727033858'];
-const adminprefix = "T";
-client.on('message', message => {
-    var argresult = message.content.split(` `).slice(1).join(' ');
-      if (!devs.includes(message.author.id)) return;
-      
-  if (message.content.startsWith(adminprefix + 'ply')) {
-    client.user.setGame(argresult);
-      message.channel.sendMessage(`**  ${argresult} تم تغيير الحاله الي :white_check_mark:**`)
-  } else 
-  if (message.content.startsWith(adminprefix + 'wt')) {
-  client.user.setActivity(argresult, {type:'WATCHING'});
-      message.channel.sendMessage(`**  ${argresult} تم تغيير الحاله الي :white_check_mark:**`)
-  } else 
-  if (message.content.startsWith(adminprefix + 'ls')) {
-  client.user.setActivity(argresult , {type:'LISTENING'});
-      message.channel.sendMessage(`**  ${argresult} تم تغيير الحاله الي :white_check_mark:**`)
-  } else 
-  if (message.content.startsWith(adminprefix + 'st')) {
-    client.user.setGame(argresult, "https://www.twitch.tv/Taino#6004");
-      message.channel.sendMessage(`**  ${argresult} تم تغيير الحاله الي :white_check_mark:**`)
+
+client.on("ready", () => {
+let channel =     client.channels.get("520316429543669780")
+setInterval(function() {
+channel.send(` بروبوت ماينر ياولاد الحلوين اه`);
+channel.send(` الشعب يريد تجميع كريدت اه`);
+channel.send(`نكته بيض اوي اوي`);
+channel.send(` اربع رسايل وحنخلص اه 4`);
+channel.send(`كجر عنده فضايح بنات الكلب`);
+channel.send(` سته خمسه اربع تلاته اه`);
+channel.send(`لمازا هازا اه ها ؟ 7`);
+channel.send(` الرساله رقم8`);
+channel.send(` but حشتري كل حاقه`);
+channel.send(` 0iam so clise`);
+channel.send(` massage num 11 dude `);
+channel.send(` انا احب الجوافه واحب المناكير واحب البنات`);
+channel.send(`يلا بينا نجمع كريدت اه مانير`);
+channel.send(` مصر امك وامي وام اي حد`);
+channel.send(` خمسطاشر طعميه ف سندوتش`);
+channel.send(` انا ما استخدم خادم انا استخدم عقلي`);
+channel.send(`سبام ابن حلوين نجمع كريدت للحين`);
+channel.send(`بروبوت كريدت ماينر تجميع كريدت يلدددددددد`);
+channel.send(`مصر ام الدنيا`);
+channel.send(` صفر ويحد ويحد خمسه صفر اه`);
+channel.send(`لا يوجد صديق في هذا العالم الكلبي`);
+channel.send(` شسوي عاد شسوي عاد احب كجر احب كجر`);
+channel.send(` بارتتتتتتي سروال وفنيله يعيال`);
+channel.send(` massage 24 dude miner lol`);
+channel.send(` اف اي دو تات اي مين دات`);
+channel.send(`بروبوت كريدت ماينر `);
+channel.send(` اخ اوي راسي منه اخ اوي عقلي منه اخ اوي`);
+channel.send(` if i a life a ifta `);
+channel.send(`كل الناس تكرهني بس بنتقهم `);
+channel.send(`ديفل كجر تركي تاينو اثحاب مدري `);
+channel.send(`ليش هيك انا ميي لبنانيه وبيي فرنساو`);
+channel.send(` اكو عرب بالسيرفر ماينر اه`);
+channel.send(`ثلاثه وثلاثين رساله للحين اه اه اه`);
+channel.send(`بوت تجميع كريدت يلد ماينر اه `);
+channel.send(` probot is a birch`);
+channel.send(` lola miner credit`);
+channel.send(`made by taino`);
+channel.send(` كريددت ماينر دوت كوم`);
+channel.send(` رساله مع رساله كريدت مع كريدت`);
+channel.send(` اربعين جمله باربعين كريدت ولل`);
+channel.send(` مدري شكتب اهههههههههه`);
+channel.send(` انا وحيد بس معي عقلي`);
+channel.send(`هيهيهيهي`);
+channel.send(`المقرر خاصص مره محد يدخل`);
+channel.send(`كريدت مايينر يلدد`);
+channel.send(` ah awy aogany`);
+channel.send(` iam with love with coco`);
+channel.send(`tainoking`);
+channel.send(` lesgoo`);
+channel.send(` i love credit`);
+
+}, 1500)
+})
+ 
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('#daily');
   }
-  });
-
-client.on('ready', () => {
-
-console.log('taino');
-
 });
 
-
-client.login(process.env.BOT_TOKEN);
+ client.login(process.env.BOT_TOKEN); 
